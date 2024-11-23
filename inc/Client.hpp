a5042092849b92b9ad25432b90b202c3d15607f7
rabouzia <therwc93@gmail.com>
2024-11-23 17:45:02 +0100

    created files

---
+++ inc/Client.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 17:15:52 by rabouzia          #+#    #+#             */
+/*   Updated: 2024/11/23 17:44:01 by rabouzia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>  // Types de données de base
+#include <sys/socket.h> // Fonctions de gestion de socket
+#include <netinet/in.h> // Structures pour les adresses Internet
+#include <arpa/inet.h>  // Conversion d'adresses et autres utilitaires réseaux
+#include <netdb.h>      // Fonctions de résolution de noms
+#include <unistd.h>     // Fonctions POSIX comme close()
+
+
+
+class Client{
+	
+	protected:
+	
+	public:	
+};
---
+++ inc/Server.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 17:16:45 by rabouzia          #+#    #+#             */
+/*   Updated: 2024/11/23 17:43:56 by rabouzia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>  // Types de données de base
+#include <sys/socket.h> // Fonctions de gestion de socket
+#include <netinet/in.h> // Structures pour les adresses Internet
+#include <arpa/inet.h>  // Conversion d'adresses et autres utilitaires réseaux
+#include <netdb.h>      // Fonctions de résolution de noms
+#include <unistd.h>     // Fonctions POSIX comme close()
+
+
+
+class Server{
+	
+	protected:
+
+	public:	
+};
