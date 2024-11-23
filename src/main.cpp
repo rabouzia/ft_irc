7958782a88e733c5b5e01fd8c033c59a9425589c
rabouzia <therwc93@gmail.com>
2024-11-23 17:46:35 +0100

    added notes

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,39 @@
 /*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 17:44:45 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/11/23 17:44:46 by rabouzia         ###   ########.fr       */
+/*   Updated: 2024/11/23 17:46:20 by rabouzia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "main.hpp"
+
+
+int main ()
+{
+
+
+	
+}
+
+/*
+
+./ircserv <port> <password>
+
+ port: The port number on which your IRC server will be listening to for incoming
+IRC connections
+• password: The connection password. It will be needed by any IRC client that tries
+to connect to your server.
+
+KICK - Eject a client from the channel
+∗ INVITE - Invite a client to a channel
+∗ TOPIC - Change or view the channel topic
+∗ MODE - Change the channel’s mode:
+· i: Set/remove Invite-only channel
+· t: Set/remove the restrictions of the TOPIC command to channel
+operators
+· k: Set/remove the channel key (password)
+· o: Give/take channel operator privilege
+· l: Set/remove the user limit to channel
+
+*/
